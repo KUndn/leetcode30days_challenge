8fb696855651616e5fbda8176905c8270b82ccba
kundan <kundan.1rn16ec057@gmail.com>
2020-04-14 21:06:52 +0530

    Create Day11_Last_Stone_weight.py

---
+++ Day11_Last_Stone_weight.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-val for val in stones]
+        heapq.heapify(stones)
+        while len(stones) > 1:
+            x1 = heapq.heappop(stones)
+            x2 = heapq.heappop(stones)
+            if x1 != x2:
+                heapq.heappush(stones,-abs(x1-x2))
+        if len(stones) == 0:
+            return 0
+        return -stones[0]
+        
