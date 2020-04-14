80cd45ba01dfd175a871520f2723017bea6c6b8d
kundan <kundan.1rn16ec057@gmail.com>
2020-04-14 20:59:30 +0530

    Create Day6_Group_Anagrams.py

---
+++ Day6_Group_Anagrams.py
@@ -0,0 +1,5 @@
+def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = defaultdict(list)
+        for word in strs:
+            anagrams[''.join(sorted(word))].append(word)
+        return list(anagrams.values())
