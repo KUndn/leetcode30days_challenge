ef1a41c7abd5fb7e101c1063a00824e4f969702b
kundan <kundan.1rn16ec057@gmail.com>
2020-04-14 20:55:55 +0530

    Create Day3_MaximumSubarray.py

---
+++ Day3_MaximumSubarray.py
@@ -0,0 +1,7 @@
+def maxSubArray(self, nums: List[int]) -> int:
+        windowSum = nums[0]
+        maxSum = nums[0]
+        for i in range(1,len(nums)):
+            windowSum = max(windowSum+nums[i], nums[i])
+            maxSum = max(windowSum, maxSum)
+        return maxSum
